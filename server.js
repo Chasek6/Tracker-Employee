d24ebab3c9115480cad987bb2e65f0ca174cc201
Chasek6 <quranstratton@gmail.com>
2022-09-13 00:27:43 -0400

    corrected the program starting error however,I still have to fix othe errors like the first argument must be a string or an instance to work

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 const inquirer = require('inquirer');
 const table = require('console.table');
 const mysql = require('mysql2');
-const {orderedSelectAll, employeeQuery, roleQuery } = require('./queries');
+const {orderedSelectAll, employeeQuery, roleQuery } = require('./helpers/queries');
 
 // create the connection to database
 
@@ -25,12 +25,12 @@ const db = mysql.createConnection({
 console.log('Connected to the company_db database.')
 );
 
-
-// function to start the application
-const init = () => {
-    console.log('Welcome to the Employee Tracker!');
+// 
+// // function to start the application
+// const init = () => {
+//     console.log('Welcome to the Employee Tracker!');
     
-}
+// }
 
 // function to view main menu
 
@@ -408,10 +408,11 @@ const exitApplication = () => {
 }
 // initialize application
 
-// const init = () => {
-//     mainMenu();
-// }
+const init = () => {
+    console.log('Welcome to the Employee Tracker!');
+    mainMenu();
+}
 
-// init();
+init();
 
 
